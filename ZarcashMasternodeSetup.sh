a598880aee4ea4049bc48128e350340a9e4a665e
DeamonProfProfSA <44237606+ProfProfcompile@users.noreply.github.com>
2019-07-14 19:15:20 +0200

    Update ZarcashMasternodeSetup.sh

--- ZarcashMasternodeSetup.sh
+++ ZarcashMasternodeSetup.sh
@@ -24,52 +24,52 @@ sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 
-echo -e "\n\nsetup zarcashd ...\n\n"
+echo -e "\n\nsetup zarhexcashd ...\n\n"
 cd ~
-wget https://github.com/ProfProfcompile/zarcash/releases/download/v1.2.3.5/ZarcashLinuxV1.2.3.5ALL.zip
+wget https://github.com/ProfProfcompile/zarhexcash/releases/download/v1.2.3.5/ZarhexcashLinuxV1.2.3.5ALL.zip
 sudo apt-get install unzip
-chmod -R 755 ZarcashLinuxV1.2.3.5ALL.zip
-unzip -o ZarcashLinuxV1.2.3.5ALL.zip
+chmod -R 755 ZarhexcashLinuxV1.2.3.5ALL.zip
+unzip -o ZarhexcashLinuxV1.2.3.5ALL.zip
 sleep 5
-mkdir /root/zarcash
-mkdir /root/.zarcash
-cp /root/zarcashd /root/zarcash
-cp /root/zarcash-cli /root/zarcash
+mkdir /root/zarhexcash
+mkdir /root/.zarhexcash
+cp /root/zarhexcashd /root/zarhexcash
+cp /root/zarhexcash-cli /root/zarhexcash
 sleep 5
-rm /root/zarcashd
-rm /root/zarcash-cli
-rm /root/zarcash-qt
-rm /root/zarcash-tx
-rm /root/ZarcashLinuxV1.2.3.5ALL.zip
-chmod -R 755 /root/zarcash
-chmod -R 755 /root/.zarcash
+rm /root/zarhexcashd
+rm /root/zarhexcash-cli
+rm /root/zarhexcash-qt
+rm /root/zarhexcash-tx
+rm /root/ZarhexcashLinuxV1.2.3.5ALL.zip
+chmod -R 755 /root/zarhexcash
+chmod -R 755 /root/.zarhexcash
 
-echo -e "\n\nlaunch zarcashd ...\n\n"
+echo -e "\n\nlaunch zarhexcashd ...\n\n"
 sudo apt-get install -y pwgen
 GEN_PASS=`pwgen -1 20 -n`
 IP_ADD=`curl ipinfo.io/ip`
 
-echo -e "rpcuser=zarcashuser\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=256\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}:40444\nstaking=0" > /root/.zarcash/zarcash.conf
-cd /root/zarcash
-./zarcashd
+echo -e "rpcuser=zarhexcashuser\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=256\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}:40444\nstaking=0" > /root/.zarhexcash/zarhexcash.conf
+cd /root/zarhexcash
+./zarhexcashd
 sleep 40
-masternodekey=$(./zarcash-cli masternode genkey)
-./zarcash-cli stop
+masternodekey=$(./zarhexcash-cli masternode genkey)
+./zarhexcash-cli stop
 
 # add launch after reboot
 crontab -l > tempcron
-echo "@reboot /root/zarcash/zarcashd -reindex >/dev/null 2>&1" >> tempcron
+echo "@reboot /root/zarhexcash/zarhexcashd -reindex >/dev/null 2>&1" >> tempcron
 crontab tempcron
 rm tempcron
 
-echo -e "masternode=1\nmasternodeprivkey=$masternodekey\n\n\n" >> /root/.zarcash/zarcash.conf
+echo -e "masternode=1\nmasternodeprivkey=$masternodekey\n\n\n" >> /root/.zarhexcash/zarhexcash.conf
 
 
-./zarcashd -daemon
-cd /root/.zarcash
+./zarhexcashd -daemon
+cd /root/.zarhexcash
 ufw allow 40444
 
 # output masternode key
 echo -e "Masternode private key: $masternodekey"
-echo -e "Welcome to the Zarcash Masternode Network!"
+echo -e "Welcome to the Zarhexcash Masternode Network!"
 
